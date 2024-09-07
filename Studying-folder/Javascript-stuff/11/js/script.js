ef429bbdcdcc528b084a56ec2a7433ee87094bf7
Vadim <shendakovv@inbox.ru>
2024-09-07 23:00:58 +0300

    Modified modal in project, looked through funtions-constructor

---
+++ Studying-folder/Javascript-stuff/11/js/script.js
@@ -0,0 +1,23 @@
+// const num = new Number(3);
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+User.prototype.exit = function() {
+    console.log(`Пользователь ${this.name} ушёл`)
+}
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
