8bb6f3fe54b7916bd446b01614f9546beaa57ae0
Vadim <shendakovv@inbox.ru>
2024-09-08 18:54:45 +0300

    This. Just This. It's quite confusing

--- Studying-folder/Javascript-stuff/11/js/script.js
+++ Studying-folder/Javascript-stuff/11/js/script.js
@@ -9,12 +9,28 @@ function User(name, id) {
     }
 }
 
+class User {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;
+    }
+    hello() {
+        console.log(`Hello ${this.name}`);
+    }
+    exit() {
+        console.log(`Пользователь ${this.name} ушёл`);
+    }
+}
+
 User.prototype.exit = function() {
     console.log(`Пользователь ${this.name} ушёл`)
 }
 
 const ivan = new User('Ivan', 28);
 const alex = new User('Alex', 20);
+
+ivan.exit();
 
 ivan.hello();
 alex.hello();
