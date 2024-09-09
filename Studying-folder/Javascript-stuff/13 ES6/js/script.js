bf2cbd3715e6f6b9fc10f4e1cfdcfb1f8e7e2f2b
Vadim <shendakovv@inbox.ru>
2024-09-09 21:31:39 +0300

    Just keep studying

--- Studying-folder/Javascript-stuff/13 ES6/js/script.js
+++ Studying-folder/Javascript-stuff/13 ES6/js/script.js
@@ -23,8 +23,21 @@ class ColoredRectangleWithText extends Rectangle {
 
 const div = new ColoredRectangleWithText(25, 10, 'Hello world', 'red');
 
-div.showMyProprs();
-console.log(div.calcArea());
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
+}
+
+log('basic','rest', 'operator', 'usage');
+
+function calcOrDouble(number, basis = 2) {
+    // basis = basis || 2;
+    console.log(number * basis);
+}
+
+calcOrDouble(3);
+
+// div.showMyProprs();
+// console.log(div.calcArea());
 
 // const square = new Rectangle(10, 10);
 // const long = new Rectangle(15, 10);
