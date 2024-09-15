a646f67c42e268ee869a8a1a4c133a9ef11badfb
Vadim <shendakovv@inbox.ru>
2024-09-15 18:22:44 +0300

    Haha being.. begin, also forgor to save

--- Studying-folder/Javascript-stuff/17/js/script.js
+++ Studying-folder/Javascript-stuff/17/js/script.js
@@ -31,10 +31,23 @@
 
 // const arr = [4, 5, 1, 3, 2, 6];
 
-// const res = arr.reduce((sum, current) => sum + current);
+// const res = arr.reduce((sum, current) => sum + current, 3);
 // console.log(res);
 
-const arr = ['apple', 'pear', 'plum'];
+// const arr = ['apple', 'pear', 'plum'];
 
-const res = arr.reduce((sum, current) => `${sum}, ${current}`);
-console.log(res);
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+const obj = {
+    ivan: 'person',
+    ann: 'person',
+    dog: 'animal',
+    cat: 'animal'
+};
+
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'person')
+.map(item => item[0]);
+
+console.log(newArr);
