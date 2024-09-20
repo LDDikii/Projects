ec8e60d58a60be82aa5fc95a3236ffb1e69b6aee
Vadim <shendakovv@inbox.ru>
2024-09-20 03:34:54 +0300

    ES6 modules

---
+++ Studying-folder/Javascript-stuff/24/src/js/script.js
@@ -0,0 +1,14 @@
+// import {one as first} from './main.js';
+import {one, two} from './main.js';
+import sayHi from './main.js';
+// import * as data from './main.js';
+
+// console.log(`${data.one} and ${data.two}`);
+
+// data.sayHi();
+
+
+// console.log(first);
+
+console.log(`${one} and ${two}`);
+sayHi();
