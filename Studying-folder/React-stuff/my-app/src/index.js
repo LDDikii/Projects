531bab3173f03f720ca3063a5f2e28420e2fd76f
Vadim <shendakovv@inbox.ru>
2024-09-23 05:55:26 +0300

    Elements and components

--- Studying-folder/React-stuff/my-app/src/index.js
+++ Studying-folder/React-stuff/my-app/src/index.js
@@ -2,24 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Header } from './App';
 
-const text = 'Hello world';
+// const text = 'Hello world';
 
-const elem = (
-  <div>
-    <h2 className='text'>Текст: {text} {2+2} {['2312314', '213123']}</h2> 
-    <input type='text'></input>
-    <label htmlFor=""></label>
-    <button tabIndex={0}>Click</button>
-  </div>
-);
+// const elem = (
+//   <div>
+//     <h2 className='text'>Текст: {text} {2+2} {['2312314', '213123']}</h2> 
+//     <input type='text'></input>
+//     <label htmlFor=""></label>
+//     <button tabIndex={0}>Click</button>
+//   </div>
+// );
 
 // const elem = React.createElement('h2', {className: 'greetings'}, 'Hello world');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem,
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>
 );
